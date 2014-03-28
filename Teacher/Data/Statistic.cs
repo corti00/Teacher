9d5e2914347fed3a395a3e6cdae6a8825bb418ad
corti00 <wienands.felix@gmail.com>
2014-03-28 22:49:29 +0100

    Abstrakte Klasse fuer StaticValue hinzugefuegt

--- Teacher/Data/Statistic.cs
+++ Teacher/Data/Statistic.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Teacher.Interfaces;
 
 namespace Teacher.Data
 {
-    class Statistic : SetStatisticValue
+    class Statistic : AStatistic
     {
     }
 }
