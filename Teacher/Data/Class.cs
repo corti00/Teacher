529f0bb8dfc56deaa4c636837ee29a52fc0f9556
corti00 <wienands.felix@gmail.com>
2014-03-28 22:07:18 +0100

    Data Klassen erstellen

---
+++ Teacher/Data/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher.Data
+{
+    
+    class Class
+    {
+        List<Student> studentList= new List<Student>();
+        String name;
+        DateTime time;
+    }
+}
